a461307d68845f3803054fd59c3320fef6df9f2f
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-08 22:26:06 -0500

    Define methods to Questions at controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,21 @@
 class QuestionsController < ApplicationController
 
+before_action :authenticate_user!, :except => [:show, :index]
+
+  def index
+    @questions = Question.all
+  end
 
   def new
     @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def show
+    @question = Question.find(params[:id])
   end
 
   def create
@@ -10,11 +23,36 @@ class QuestionsController < ApplicationController
   @question.user = current_user
   if @question.save
    flash[:success] = "Pregunta publicada exitosamente"
-      redirect_to questions_path
+      redirect_to questions_path(@question)
     else
-       flash[:danger] = "Debes llenar todos los datos"
+       flash[:danger] = "#{@question.errors.messages}"
        redirect_to new_question_path
      end
+   end
+
+   def update
+    @question = Question.find(params[:id])
+    if @question.update(questions_params)
+      flash[:notice] = "Tu pregunta ha sido modificada"
+        redirect_to questions_path(@question)
+    else
+      flash[:danger] = "#{@question.errors.messages}"
+        render :edit
+    end
+  end
+
+   def destroy
+      question =Question.find(params[:id])
+      question.destroy
+
+       redirect_to questions_path
+  end
+
+
+ private
+
+   def questions_params
+     params.require(:question).permit(:title, :description, :id)
    end
 
 end

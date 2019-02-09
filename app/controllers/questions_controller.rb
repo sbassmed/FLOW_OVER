bcb8a468c68475177fef75efc45ebb655dbfafc1
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-08 22:26:06 -0500

    Define create and new methods into Question Controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
 class QuestionsController < ApplicationController
+
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+  @question = Question.create(questions_params)
+  @question.user = current_user
+  if @question.save
+   flash[:success] = "Pregunta publicada exitosamente"
+      redirect_to questions_path
+    else
+       flash[:danger] = "Debes llenar todos los datos"
+       redirect_to new_question_path
+     end
+   end
+
 end
